1585ae61b046587ea4ae690d44274b821c200f55
TopsDarshana <darshanarana1533@gmail.com>
2024-07-15 00:23:02 +0530

    q3.c-q5.c is done

---
+++ SE/projects/q4.c
@@ -0,0 +1,3 @@
+// What is DFD ? 
+
+/*ANS: DFD stands for Data Flow Diagram, a graphical representation of the flow of data through a system. It's a tool used in systems analysis and design to visualize the flow of data and the processes that transform it.*/
---
+++ SE/q5.c
@@ -0,0 +1,3 @@
+// What is flow chart ?
+
+/*ANS: A flowchart is a graphical representation of a process or a workflow, showing the steps and their sequence by using various symbols. It is a valuable tool for both analyzing and designing processes, making complex procedures easier to understand.*/
