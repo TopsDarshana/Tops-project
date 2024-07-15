91b713388ad9e2cd5f1c118c7af8a54ff47dfe95
TopsDarshana <darshanarana1533@gmail.com>
2024-07-15 20:40:28 +0530

    q6.c is done

---
+++ SE/projects/Module1/q6.c
@@ -0,0 +1,3 @@
+// What is use case diagram ? Create a use-case on bill payment
+
+/*ANS: A use case diagram is a visual representation of the functional requirements of a system. It shows the interactions between users and the system to achieve specific goals. */
