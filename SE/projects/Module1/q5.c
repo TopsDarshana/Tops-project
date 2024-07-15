5c16fdad1a4596eb8233d83fe6dc7a6eb0436815
TopsDarshana <darshanarana1533@gmail.com>
2024-07-15 21:28:47 +0530

    Module 1,q5.c is done

---
+++ SE/projects/Module1/q5.c
@@ -0,0 +1,5 @@
+// What is flow chart ? Create a flowchart to make additon of two numbers.
+
+/*ans: Flow chart is define process.
+It is represent the sitiuation graphically.
+It uses various symbols to represent different types of actions or steps in a process and shows the flow of control from one step to the next.*/
--- SE/q5.c
+++
@@ -1,3 +0,0 @@
-// What is flow chart ?
-
-/*ANS: A flowchart is a graphical representation of a process or a workflow, showing the steps and their sequence by using various symbols. It is a valuable tool for both analyzing and designing processes, making complex procedures easier to understand.*/
